26eb59e10de68e96e6f5f58d918a984417753194
chdonncha <chdonncha@gmail.com>
2016-10-23 18:09:07 +0100

    fixed bugs

--- Lab3/Example/lab3q4.sql
+++ Lab3/Example/lab3q4.sql
@@ -1,3 +1,6 @@
+DROP TABLE TEAMS CASCADE CONSTRAINTS PURGE;
+DROP TABLE MATCHES CASCADE CONSTRAINTS PURGE;
+
 create table TEAMS(
   TeamID         integer NOT NULL,
   TeamName       varchar(50) NULL,
@@ -13,8 +16,8 @@ create table MATCHES(
   Goal_B        integer NULL,
   Competition   varchar(100) NULL,
   PRIMARY KEY(MatchID),
-  CONSTRAINT R_1 FOREIGN KEY (TeamA_ID) REFERENCES TEAMS (TeamA_ID),
-  CONSTRAINT R_2 FOREIGN KEY (TeamB_ID) REFERENCES TEAMS (TeamB_ID)
+  CONSTRAINT R_1 FOREIGN KEY (TeamA_ID) REFERENCES TEAMS (TeamID),
+  CONSTRAINT R_2 FOREIGN KEY (TeamB_ID) REFERENCES TEAMS (TeamID)
 );
 
 insert into TEAMS values(1, 'Arsenal', 'England');
@@ -25,3 +28,4 @@ insert into TEAMS values(5, 'Barcellona', 'Spain');
 insert into TEAMS values(6, 'Real Madrid', 'Spain');
 insert into TEAMS values(7, 'Getafe', 'Spain');
 insert into TEAMS values(8, 'Sevilla', 'Spain');
+
