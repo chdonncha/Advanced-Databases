8081bb4430c3557628808c6024747a55f476c890
chdonncha <chdonncha@gmail.com>
2016-12-08 23:33:48 +0000

    relationship updates

---
+++ Labs/Lab7/lab1.js
@@ -0,0 +1,124 @@
+db.lab1.insert(student={
+	Student_ID: '1',
+	Name: 'Mary',
+	Surname: 'Murray',
+	Nationality: 'Irish',
+	Age: '45',
+	courses: [
+	{
+		Student_ID: '1',
+		Course_ID: 'DB',
+		Mark: '56',
+		ExamDate: '10/10/2011'
+	},
+	{
+		Student_ID: '1',
+		Course_ID: 'MA',
+		Mark: '76',
+		ExamDate: '09/11/2012'
+	},
+	{
+		Student_ID: '1',
+		Course_ID: 'PR',
+		Mark: '45',
+		ExamDate: '02/07/2014'
+	}
+	]
+});
+
+db.lab1.insert(student={
+	Student_ID: '2',
+	Name: 'Bill',
+	Surname: 'Bellichick',
+	Nationality: 'American',
+	Age: '32',
+	courses: [	
+	{
+		Student_ID: '2',
+		Course_ID: 'DB',
+		Mark: '55',
+		ExamDate: '10/10/2011'
+	},
+	{
+		Student_ID: '2',
+		Course_ID: 'MA',
+		Mark: '87',
+		ExamDate: '09/11/2011'
+	},
+	{
+		Student_ID: '2',
+		Course_ID: 'PR',
+		Mark: '45',
+		ExamDate: '10/10/2011'
+	}
+	]
+});
+
+db.lab1.insert(student={
+	Student_ID: '3',
+	Name: 'Tom',
+	Surname: 'Brady',
+	Nationality: 'Canadian',
+	Age: '22',
+	courses: [
+	{
+		Student_ID: '3',
+		Course_ID: 'DB',
+		Mark: '34',
+		ExamDate: '09/11/2012'
+	},
+	{
+		Student_ID: '3',
+		Course_ID: 'MA',
+		Mark: '56',
+		ExamDate: '02/07/2014'
+	}
+	]
+});
+
+db.lab1.insert(student={
+	Student_ID: '4',
+	Name: 'John',
+	Surname: 'Bale',
+	Nationality: 'English',
+	Age: '24',
+	courses: [
+	{
+		Student_ID: '4',
+		Course_ID: 'DB',
+		Mark: '71',
+		ExamDate: '10/10/2011'
+	},
+	{
+		Student_ID: '4',
+		Course_ID: 'MA',
+		Mark: '88',
+		ExamDate: '10/10/2011'
+	},
+	{
+		Student_ID: '4',
+		Course_ID: 'PR',
+		Mark: '95',
+		ExamDate: '09/11/2012'
+	}
+	]
+});
+
+db.lab1.insert(student={
+	Course_ID: 'DB',
+	Course_Name: 'Databases',
+	Credits: '10'
+});
+
+db.lab1.insert(student={
+	Course_ID: 'MA',
+	Course_Name: 'Maths',
+	Credits: '5'
+});
+
+db.lab1.insert({
+	Course_ID: 'PR',
+	Course_Name: 'Programming',
+	Credits: '15'
+});
+
