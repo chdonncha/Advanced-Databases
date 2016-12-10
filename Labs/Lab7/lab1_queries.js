6ac43006115a9f8ce72b25d9cec52c2ff946bf17
chdonncha <chdonncha@gmail.com>
2016-12-10 18:10:44 +0000

    final updates to lab

--- Labs/Lab7/lab1_queries.js
+++ Labs/Lab7/lab1_queries.js
@@ -13,8 +13,19 @@ db.lab1_query1.insert(
 db.lab1_query2.insert(
 	db.lab1.aggregate(
 		{$unwind : '$Courses'},
-		{$project : {Course_ID : 1, 'Courses.Mark': 1, 'Courses.Course_ID' : 1}},
-		{$match : {'Courses.Mark' : {$gt: 40}}},
+		{$match : {'Courses.Mark' : {$gte: 40}}},
 		{$group : {_id : "$Courses.Course_ID", Count : {$sum : 1}}}
+	).toArray()
+);
+
+db.lab1_query3.insert(
+	db.lab1.aggregate(
+		{$unwind : '$Courses'},
+		{$group : {_id : "$Student_ID", 
+			Name: {$first : '$Name'}, 
+			Surname : {$first : '$Surname'},  
+			avgAmount: { $avg : '$Courses.Mark' }}},
+		{$sort : {'avgAmount' : -1}},
+		{$limit : 1}
 	).toArray()
 );
