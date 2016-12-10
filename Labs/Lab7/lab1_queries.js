2e15c5e6598a618d0b9e15fd469807761d0c2730
chdonncha <chdonncha@gmail.com>
2016-12-10 16:47:49 +0000

    finished question 2 of lab 1 finished

--- Labs/Lab7/lab1_queries.js
+++ Labs/Lab7/lab1_queries.js
@@ -1,8 +1,20 @@
-db.lab1.find(
-	{ 'courses' : 
-	{$elemMatch :
-		{'Mark' : 
-		{'$gt' : '50' 
-	}
-}}}
+db.lab1_query1.insert(
+    db.lab1.find({
+        'Courses': {
+            $elemMatch: {
+                'Mark': {
+                    $lt: 40
+                }
+            }
+        }
+    }).toArray()
+);
+
+db.lab1_query2.insert(
+	db.lab1.aggregate(
+		{$unwind : '$Courses'},
+		{$project : {Course_ID : 1, 'Courses.Mark': 1, 'Courses.Course_ID' : 1}},
+		{$match : {'Courses.Mark' : {$gt: 40}}},
+		{$group : {_id : "$Courses.Course_ID", Count : {$sum : 1}}}
+	).toArray()
 );
