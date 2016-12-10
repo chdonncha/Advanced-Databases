4a4c208180fe54520e8ace2327530b343d2ba94a
chdonncha <chdonncha@gmail.com>
2016-12-10 16:07:29 +0000

    changed data types

--- Labs/Lab7/lab1.js
+++ Labs/Lab7/lab1.js
@@ -1,115 +1,115 @@
 db.lab1.insert(
 	[
 	{
-		Student_ID: '1',
+		Student_ID: 1,
 		Name: 'Mary',
 		Surname: 'Murray',
 		Nationality: 'Irish',
-		Age: '45',
+		Age: 45,
 		courses: [
 		{
 			Course_ID: 'DB',
-			Mark: '56',
+			Mark: 56,
 			ExamDate: new Date('10/10/2011'),
 			Course_Name: 'Databases',
-			Credits: '10'
+			Credits: 10
 		},
 		{
 			Course_ID: 'MA',
-			Mark: '76',
+			Mark: 76,
 			ExamDate: new Date('09/11/2012'),
 			Course_Name: 'Maths',
-			Credits: '5'
+			Credits: 5
 		},
 		{
 			Course_ID: 'PR',
-			Mark: '45',
+			Mark: 45,
 			ExamDate: new Date('02/07/2014'),
 			Course_Name: 'Programming',
-			Credits: '15'
+			Credits: 15
 		}
 		]
 	},
 	{
-		Student_ID: '2',
+		Student_ID: 2,
 		Name: 'Bill',
 		Surname: 'Bellichick',
 		Nationality: 'American',
-		Age: '32',
+		Age: 32,
 		courses: [	
 		{
 			Course_ID: 'DB',
-			Mark: '55',
+			Mark: 55,
 			ExamDate: new Date('10/10/2011'),
 			Course_Name: 'Databases',
-			Credits: '10'
+			Credits: 10
 		},
 		{
 			Course_ID: 'MA',
-			Mark: '87',
+			Mark: 87,
 			ExamDate: new Date('09/11/2011'),
 			Course_Name: 'Maths',
-			Credits: '5'
+			Credits: 5
 		},
 		{
 			Course_ID: 'PR',
-			Mark: '45',
+			Mark: 45,
 			ExamDate: new Date('10/10/2011'),
 			Course_Name: 'Programming',
-			Credits: '15'
+			Credits: 15
 		}
 		]
 	},
 	{
-		Student_ID: '3',
+		Student_ID: 3,
 		Name: 'Tom',
 		Surname: 'Brady',
 		Nationality: 'Canadian',
-		Age: '22',
+		Age: 22,
 		courses: [
 		{
 			Course_ID: 'DB',
-			Mark: '34',
+			Mark: 34,
 			ExamDate: new Date('09/11/2012'),
 			Course_Name: 'Databases',
-			Credits: '10'
+			Credits: 10
 		},
 		{
 			Course_ID: 'MA',
-			Mark: '56',
+			Mark: 56,
 			ExamDate: new Date('02/07/2014'),
 			Course_Name: 'Maths',
-			Credits: '5'
+			Credits: 5
 		}
 		]
 	},
 	{
-		Student_ID: '4',
+		Student_ID: 4,
 		Name: 'John',
 		Surname: 'Bale',
 		Nationality: 'English',
-		Age: '24',
+		Age: 24,
 		courses: [
 		{
 			Course_ID: 'DB',
-			Mark: '71',
+			Mark: 71,
 			ExamDate: new Date('10/10/2011'),
 			Course_Name: 'Databases',
-			Credits: '10'
+			Credits: 10
 		},
 		{
 			Course_ID: 'MA',
-			Mark: '88',
+			Mark: 88,
 			ExamDate: new Date('10/10/2011'),
 			Course_Name: 'Maths',
-			Credits: '5'
+			Credits: 5
 		},
 		{
 			Course_ID: 'PR',
-			Mark: '95',
+			Mark: 95,
 			ExamDate: new Date('09/11/2012'),
 			Course_Name: 'Programming',
-			Credits: '15'
+			Credits: 15
 		}
 		]
 	}
