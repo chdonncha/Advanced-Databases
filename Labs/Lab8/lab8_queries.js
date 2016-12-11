d327912c4a8e975d6a0a7617f83234a375f6f67d
chdonncha <chdonncha@gmail.com>
2016-12-11 00:55:36 +0000

    more questions answered

--- Labs/Lab8/lab8_queries.js
+++ Labs/Lab8/lab8_queries.js
@@ -41,9 +41,19 @@ db.teams.find().sort({
 }).limit(1);
 
 
-// q 4. Update the number of goal of all the Real Madrid Players by 3 goals each
+// q 4. Update the number of goal of all the Real Madrid Players by 3 goals each **NOT WORKING**
+ 
+db.teams.update(
+	{
+		team_id : 'Real Madrid'
+	},
+	{$inc : {'$players.goal' : 3}},
+	{multi: true}
+)
 
 // q 5. using a cursor, update the number of caps of all the "Serie A" teams by incrementing them by 10% (round it!)
+
+var cursor = db.teams.find ( { type : 'example' });
 
 // q 7. Find all the players over 30 years old containing the string "es"
 
@@ -56,10 +66,27 @@ db.teams.aggregate(
 
 // q 8. Using aggregate mongoDB operator, list the total point by league.
 
+db.teams.aggregate(
+    {$group: {_id: null, total: {$sum : '$points'}}}
+);
+
 // q 9. Using aggregation, list all the teams by number of goals in descending order.
+
+db.teans.aggregate(
+	{$unwind : '$players'}
+)
 
 // q 10. Compute the average number of goal per match per player and store the output in a collection 
 // named student_id_avg_goals.  
+
+db.teams.aggregate(
+	{$unwind : '$players'},
+	{$project : {avgAmount: { $avg : ['$players.goal', '$players.caps'] }}},
+	{$group : {_id : "$players.p_id", 
+		Matches : {$first : '$players.caps'},
+		avgAmount: { $avg : ['$players.goal', '$players.caps'] }}},
+	{$sort : {'avgAmount' : -1}}
+);
 
 // q 10. Write a js function old_vs_young(x) , that receives a positive integer x representing the age of a player
 // and returns 1 if the total number of goals scored by the players with age >= x years is greater than the 
